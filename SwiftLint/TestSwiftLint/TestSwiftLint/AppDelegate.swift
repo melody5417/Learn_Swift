b8251ce34e7f60c11dfb2a3aa146a10cc18d78d8
yiqiwang(王一棋) <yiqiwang@tencent.com>
2018-06-26 18:26:58 +0800

    【ADD】swiftlint to style swift

---
+++ SwiftLint/TestSwiftLint/TestSwiftLint/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TestSwiftLint
+//
+//  Created by yiqiwang(王一棋) on 2018/6/26.
+//  Copyright © 2018年 yiqiwang(王一棋). All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ SwiftLint/TestSwiftLint/TestSwiftLint/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TestSwiftLint
+//
+//  Created by yiqiwang(王一棋) on 2018/6/26.
+//  Copyright © 2018年 yiqiwang(王一棋). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
